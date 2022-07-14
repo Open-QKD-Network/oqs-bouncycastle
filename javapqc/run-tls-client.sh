5bd86b2d0b6b5f675194a460230e0d65a6a2dcbb
Kaiduan Xie <kaiduanx@goodstartsoft.com>
2022-07-14 18:58:06 -0400

    Test Java client side hybrid key exchange.

--- javapqc/run-tls-client.sh
+++ javapqc/run-tls-client.sh
@@ -1,1 +1,2 @@
-java -Djava.util.logging.config.file=logging.properties -cp target/javapqc-0.0.1-SNAPSHOT.jar:../../lib/* com.goodstartsoft.pqc.TLSClient
+java -Djava.util.logging.config.file=logging.properties -Djdk.tls.namedGroups=p256_frodo640aes -cp target/javapqc-0.0.1-SNAPSHOT.jar:../../lib/* com.goodstartsoft.pqc.TLSClient
+#java -Djava.util.logging.config.file=logging.properties -Djdk.tls.namedGroups=secp256r1 -cp target/javapqc-0.0.1-SNAPSHOT.jar:../../lib/* com.goodstartsoft.pqc.TLSClient
--- javapqc/run-tls-server-p256-frodo640aes.sh
+++ javapqc/run-tls-server-p256-frodo640aes.sh
@@ -1,2 +1,1 @@
 java -Djava.util.logging.config.file=logging.properties -Djdk.tls.namedGroups=p256_frodo640aes -cp target/javapqc-0.0.1-SNAPSHOT.jar:../../lib/* com.goodstartsoft.pqc.TLSServer
-#java -Djava.util.logging.config.file=logging.properties -Djdk.tls.namedGroups=secp256r1 -cp target/javapqc-0.0.1-SNAPSHOT.jar:../../lib/* com.goodstartsoft.pqc.TLSServer
